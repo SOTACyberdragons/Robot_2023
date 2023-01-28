f856189e22954ed5dfdfa012a9902ab20982bf14
John C Fox <91802993+ernesto5700@users.noreply.github.com>
2023-01-28 12:19:06 -0800

    removed unused subsystems

--- src/main/cpp/subsystem_cpp/Feeder.cpp
+++
@@ -1,9 +0,0 @@
-#include "Subsystem_Headers/Feeder.h"
-
-bool cb::Feeder::getBreakBeam() {
-    return m_breakBeam.Get();
-}
-
-void cb::Feeder::setPower(double power) {
-    m_feederMotor.Set(ControlMode::PercentOutput, power);
-}
--- src/main/cpp/subsystem_cpp/Intake.cpp
+++
@@ -1,5 +0,0 @@
-#include "subsystem_headers/Intake.h"
-
-void cb::Intake::setPower(double power) {
-    intakeMotor.Set(ControlMode::PercentOutput, 0);
-}
