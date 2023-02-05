fb814ea2e1160de9ce61adabebb67e8afa7ed582
walter298 <walterstein456@gmail.com>
2023-02-04 17:55:46 -0800

    added s path option

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -1,9 +1,8 @@
 #include "RobotContainer.h"
 
 void cb::addAutoModeOptions() {
-    autoChooser.AddOption("Forward 5 meter drive", std::make_unique<DriveForward>(g_drivetrain, 5_m));
-    autoChooser.AddOption("Backward 5 meter drive", std::make_unique<DriveForward>(g_drivetrain, -5_m));
-
+    autoChooser.AddOption("S path", cb::driveClimb());
+    
     frc::SmartDashboard::PutData("Autonomous Modes", &autoChooser);
 }
 
